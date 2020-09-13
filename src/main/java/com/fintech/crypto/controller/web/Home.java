e72ed03f0def044e080db83132a4cb565b8fe266
akindutire <akinsamuel33@gmail.com>
2020-09-13 11:56:45 +0100

    push something

---
+++ src/main/java/com/fintech/crypto/controller/web/Home.java
@@ -0,0 +1,14 @@
+package com.fintech.crypto.controller.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class Home {
+    @GetMapping
+    public void index(){
+        System.out.println("I got home");
+    }
+}
