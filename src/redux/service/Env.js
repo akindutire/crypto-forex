4f6363bdb37b87230aa469262bdb58f161a6fd52
akindutire <akinsamuel33@gmail.com>
2020-09-25 00:56:09 +0100

    update

--- src/redux/service/Env.js
+++ src/redux/service/Env.js
@@ -1,3 +1,3 @@
-const prod = prod;
+const prod = false;
 const domain = prod ? 'https://crypto-forex-backend.herokuapp.com/' : 'http://localhost:5000/';
 export const getDomain = () => { return domain; }
